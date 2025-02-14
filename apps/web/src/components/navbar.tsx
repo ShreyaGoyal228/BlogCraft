e945848fc7dabf8f060497110025ca5ce20b8f60
Shreya Goyal <goyalshreya228@gmail.com>
2025-02-14 11:59:40 +0530

    small fixes

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,40 +1,44 @@
-'use client'
+"use client";
 import { Hexagon } from "lucide-react";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
-type INavbar={
-  username:string;
-
-}
-const Navbar = ({username}:INavbar) => {
-
-  const handleClick=async()=>
-  {
-    await signOut({callbackUrl: "/auth/register" });
-  
-}
+type INavbar = {
+  username: string;
+};
+const Navbar = ({ username }: INavbar) => {
+  const handleClick = async () => {
+    await signOut({ callbackUrl: "/auth/register" });
+  };
 
   return (
     <>
-      <div className="w-full bg-neutral my-5 ">
+      <div className="my-5 w-full bg-neutral ">
         <div className="container mx-auto">
-       <div className="flex items-center justify-between">
-         <div className="">
-           <Link href='/'><Hexagon /></Link>
-          </div>
-          <div className="flex items-center">
-         <div className="text-xs md:text-sm font-semibold">{`Welcome ${username}`}</div>
-       
-            
-            <Link className="btn btn-ghost md:text-sm text-xs" href='/create'>Create Post</Link>
-          
-            
-   
-          <div onClick={handleClick} className="cursor-pointer text-xs md:text-sm font-semibold">Logout</div>
+          <div className="flex items-center justify-between">
+            <div className="">
+              <Link href="/">
+                <Hexagon className="text-white" />
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <div className="text-xs font-semibold text-white md:text-sm">{`Welcome ${username}`}</div>
+
+              <Link
+                className="btn btn-ghost text-xs text-white md:text-sm"
+                href="/create"
+              >
+                Create Post
+              </Link>
+
+              <div
+                onClick={handleClick}
+                className="cursor-pointer text-xs font-semibold text-white md:text-sm"
+              >
+                Logout
+              </div>
+            </div>
           </div>
         </div>
-        </div>
-  
       </div>
     </>
   );
--- apps/web/src/env.js
+++ apps/web/src/env.js
@@ -15,15 +15,10 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string()
         : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
-    ),
-    GOOGLE_CLIENT_ID:z.string(),
-    GOOGLE_CLIENT_SECRET:z.string(),
+    // NEXTAUTH_URL: z.preprocess(
+    //   (str) => process.env.VERCEL_URL ?? str,
+    //   process.env.VERCEL ? z.string() : z.string().url()
+    // ),
  
   },
 
@@ -44,9 +39,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   
   },
   /**
